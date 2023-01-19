93b414c8d62b416cc4ba2045c924383c123aaeb1
Dan Liebault <dan.liebault@viacesi.fr>
2023-01-19 23:51:58 +0100

    init

---
+++ app/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <Page>
+        <TabView class="tab-view">
+            <TabViewItem title="Home" iconSource="res://home">
+                <StackLayout class="p-20">
+                    <Label text="Home Tab Content" class="h2 text-center" />
+                </StackLayout>
+            </TabViewItem>
+            <TabViewItem title="Settings" iconSource="res://settings">
+                <StackLayout class="p-20">
+                    <Label text="Settings Tab Content" class="h2 text-center" />
+                </StackLayout>
+            </TabViewItem>
+        </TabView>
+    </Page>
+</template>
+  
+<script lang="ts">
+import Vue from "nativescript-vue";
+import Component from "vue-class-component";
+
+@Component
+export default class TabNavigation extends Vue { }
+</script>
+  
+<style scoped>
+.tab-view {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+}
+</style>
