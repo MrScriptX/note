5d4369a2f44729de648973d034fd135d169ef953
Dan Liebault <d.liebault@systancia.com>
2023-01-20 12:38:20 +0100

    icons

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,13 @@
 import Vue from 'nativescript-vue'
 import Home from './components/Home.vue'
+import Vuex from 'vuex'
 
 declare let __DEV__: boolean;
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = !__DEV__
+
+Vue.use(Vuex)
 
 new Vue({
   render: (h) => h('frame', [h(Home)]),
